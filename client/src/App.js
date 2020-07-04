109205bc0f9d59ac032df47e2cf8859579697856
marcoste <marcostesvk@gmail.com>
2020-07-04 20:19:31 +0200

    Added leaflet map

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './Components/FlagMap';
-import FlagMap from './Components/FlagMap';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Icon } from 'leaflet';
+import * as mapData from './data/places.json';
+import './app.css';
 
-function App() {
-	return <FlagMap></FlagMap>;
+export default function App() {
+	const [activePark, setActivePark] = React.useState(null);
+
+	return (
+		<Map center={[45.420422, -75.692429]} zoom={12}>
+			<TileLayer
+				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+			/>
+
+			{mapData.features.map((place) => (
+				<Marker
+					key={place.properties.PARK_ID}
+					position={[
+						place.geometry.coordinates[1],
+						place.geometry.coordinates[0],
+					]}
+				></Marker>
+			))}
+		</Map>
+	);
 }
-
-export default App;
