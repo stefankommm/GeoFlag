0824b6ecfa1e5917224c1163d917a29932a44588
marcoste <marcostesvk@gmail.com>
2020-07-04 21:54:45 +0200

    fixed cors in express, added fetching flags from own API

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import { Icon } from 'leaflet';
-import * as mapData from './data/places.json';
+import useSwr from 'swr';
 import './app.css';
 
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
+
 export default function App() {
-	const [activePark, setActivePark] = React.useState(null);
+	const [activeFlag, setActiveFlag] = React.useState(null);
+	const url = 'http://localhost:5000/api/flags';
+	const { data, error } = useSwr(url, { fetcher });
+	const flags = data ? data : [];
 
 	return (
 		<Map center={[45.420422, -75.692429]} zoom={12}>
@@ -14,15 +19,30 @@ export default function App() {
 				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 			/>
 
-			{mapData.features.map((place) => (
+			{flags.map((flag) => (
 				<Marker
-					key={place.properties.PARK_ID}
-					position={[
-						place.geometry.coordinates[1],
-						place.geometry.coordinates[0],
-					]}
+					key={flag._id}
+					position={[flag.curloc[1], flag.curloc[0]]}
+					onclick={() => {
+						setActiveFlag(flag);
+					}}
 				></Marker>
 			))}
+
+			{activeFlag && (
+				<Popup
+					position={[activeFlag.curloc[1], activeFlag.curloc[0]]}
+					onClose={() => {
+						setActiveFlag(null);
+					}}
+				>
+					<div>
+						<h2>{activeFlag.name}</h2>
+						<p>{activeFlag.name}</p>
+					</div>
+					🇸🇰🇸🇰🇸🇰
+				</Popup>
+			)}
 		</Map>
 	);
 }
--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+var cors = require('cors');
 const connectDB = require('./config/db');
 const app = express();
 
 //Connect Database
 connectDB();
+
+const allowedOrigins = ['http://localhost:3000'];
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				const msg =
+					'The CORS policy for this site does not allow access from the specified Origin.';
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		},
+	})
+);
 
 //Init Middleware
 app.use(express.json({ extended: false }));
