78239a79b30db98f429f601b3397a9ef75e7e17e
marcoste <marcostesvk@gmail.com>
2020-07-11 19:47:36 +0200

    Added new design, fix few imports, nev navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,15 @@
 import React, { useEffect } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import './app.css';
 import Axios from 'axios';
-
-const fetcher = (...args) => fetch(...args).then((response) => response.json());
+import './leaflet.css';
+import FlagMap from './../../client/src/Components/FlagMap';
+import TopBar from './Components/TopBar';
 
 export default function App() {
-	const [activeFlag, setActiveFlag] = React.useState(null);
-	const [fetchedFlags, setFetchedFlags] = React.useState([]);
-
-	const url = 'http://localhost:5000/api/flags';
-	//const { data, error } = useSwr(url, { fetcher });
-	//const flags = data ? data : [];
-
-	useEffect(async () => {
-		const res = await Axios('http://localhost:5000/api/flags');
-		console.log(res);
-		setFetchedFlags(res.data);
-	}, []);
-
 	return (
-		<Map center={[48.987427, 19.090228]} zoom={7}>
-			<TileLayer
-				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-			/>
-
-			{fetchedFlags.map((flag) => (
-				<Marker
-					key={flag._id}
-					position={[flag.curloc.split(', ')[0], flag.curloc.split(', ')[1]]}
-					onclick={() => {
-						setActiveFlag(flag);
-					}}
-				></Marker>
-			))}
-
-			{activeFlag && (
-				<Popup
-					position={[
-						activeFlag.curloc.split(', ')[0],
-						activeFlag.curloc.split(', ')[1],
-					]}
-					onClose={() => {
-						setActiveFlag(null);
-					}}
-				>
-					<div>
-						<h2>{activeFlag.name}</h2>
-						<p>{activeFlag.name}</p>
-						<p>MADE BY: {activeFlag.user}</p>
-					</div>
-				</Popup>
-			)}
-		</Map>
+		<div>
+			<TopBar />
+			<FlagMap></FlagMap>
+		</div>
 	);
 }
--- client/src/Components/FlagMap.js
+++ client/src/Components/FlagMap.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import './../leaflet.css';
+import Axios from 'axios';
 
-const position = [51.505, -0.09];
-const map = (
-	<Map center={position} zoom={13}>
-		<TileLayer
-			url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-			attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-		/>
-		<Marker position={position}>
-			<Popup>
-				A pretty CSS3 popup.
-				<br />
-				Easily customizable.
-			</Popup>
-		</Marker>
-	</Map>
-);
+export default function FlagMap() {
+	const [activeFlag, setActiveFlag] = React.useState(null);
+	const [fetchedFlags, setFetchedFlags] = React.useState([]);
 
-export default map;
+	const url = 'http://localhost:5000/api/flags';
+	//const { data, error } = useSwr(url, { fetcher });
+	//const flags = data ? data : [];
+
+	useEffect(async () => {
+		const res = await Axios(url);
+		console.log(res);
+		setFetchedFlags(res.data);
+	}, []);
+
+	return (
+		<Map center={[48.987427, 19.090228]} zoom={7}>
+			<TileLayer
+				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+			/>
+
+			{fetchedFlags.map((flag) => (
+				<Marker
+					key={flag._id}
+					position={[flag.curloc.split(', ')[0], flag.curloc.split(', ')[1]]}
+					onclick={() => {
+						setActiveFlag(flag);
+					}}
+				></Marker>
+			))}
+
+			{activeFlag && (
+				<Popup
+					position={[
+						activeFlag.curloc.split(', ')[0],
+						activeFlag.curloc.split(', ')[1],
+					]}
+					onClose={() => {
+						setActiveFlag(null);
+					}}
+				>
+					<div>
+						<h2>{activeFlag.name}</h2>
+						<p>{activeFlag.name}</p>
+						<p>MADE BY: {activeFlag.user}</p>
+					</div>
+				</Popup>
+			)}
+		</Map>
+	);
+}
